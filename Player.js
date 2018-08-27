42484d2bf2155ad204bca4f22564fd2725c0d5f2
GaborMartin <gabormartin94@gmail.com>
2018-08-27 11:15:16 +0200

    Added logic to betRequest method

--- Player.js
+++ Player.js
@@ -4,12 +4,32 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    console.log(gameState.community_cards);
+    let myPlayer = this.getMyPlayer(gameState);
+    console.log(myPlayer);
+    let community_cards = gameState.community_cards;
     let cards = gameState.players;
-    for (let i = 0; i < cards.length; i++) {
-      console.log(cards[i]);
+    let counter = 0;
+    for (let i = 0; i < community_cards.length; i++) {
+      for (let j = 0; i < cards.length; i++) {
+        if (community_cards[i].rank == cards[j].rank) {
+          counter += 1;
+        }
+      }
+    }
+    if (counter > 1) {
+      bet(300);
+    } else if (counter == 4) {
+      bet(myPlayer.stack);
     }
     bet(150);
+  }
+
+  static getMyPlayer(gameState) {
+    for (let i = 0; 0 < gameState.players.length; i ++) {
+      if (gameState.player[i].name == "Pokermon") {
+        return player = gameState.player[i];
+      }
+    }
   }
 
   static showdown(gameState) {
