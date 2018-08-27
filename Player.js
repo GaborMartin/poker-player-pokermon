778ccd55cd0dbd7a8bbae7e92609e227d770a4ba
GaborMartin <gabormartin94@gmail.com>
2018-08-27 13:54:59 +0200

    Refactored logic

--- Player.js
+++ Player.js
@@ -11,31 +11,46 @@ class Player {
     console.log(community_cards);
     let cards = myPlayer.hole_cards;
     console.log(cards);
-    let counter = 0;
-
-    if (community_cards.length > 0) {
-      for (let i = 0; i < community_cards.length; i++) {
-        for (let j = 0; j < cards.length; j++) {
-          if (community_cards[i].rank == cards[j].rank) {
-            counter += 1;
-          }
-        }
-      }
-      if (counter > 1) {
+    
+    // if no community cards
+    if (community_cards.length == 0) {
+      if (myPlayer.hole_cards[0].rank == myPlayer.hole_cards[1].rank || myPlayer.hole_cards[0].rank=='J' || myPlayer.hole_cards[0].rank=='Q' ||myPlayer.hole_cards[0].rank=='K' ||myPlayer.hole_cards[0].rank=='A' || myPlayer.hole_cards[1].rank=='J' || myPlayer.hole_cards[1].rank=='Q'|| myPlayer.hole_cards[1].rank=='K'|| myPlayer.hole_cards[1].rank=='A' ){
         bet(gameState.current_buy_in-myPlayer.bet + 10);
-      } else if (counter == 4) {
+      } 
+    }
+
+    // If flop turn river.
+    else if (community_cards.length > 0) {
+      if (this.countPairs() == 4) {
         bet(myPlayer.stack);
+      } else if (this.countPairs() == 3) {
+        bet(myPlayer.stack);
+      } else if (this.countPairs() == 2) {
+        bet(gameState.current_buy_in-myPlayer.bet + gameState.current_buy_in-myPlayer.bet / 2);
+      } else if (this.countPairs() == 1) {
+        bet(gameState.current_buy_in-myPlayer.bet + 10);
       }
-      
-      console.log(gameState.current_buy_in-myPlayer.bet + 10);
+    } else {
+      bet(0);
     }
-    if (myPlayer.hole_cards[0].rank == myPlayer.hole_cards[1].rank || myPlayer.hole_cards[0].rank=='J' || myPlayer.hole_cards[0].rank=='Q' ||myPlayer.hole_cards[0].rank=='K' ||myPlayer.hole_cards[0].rank=='A' || myPlayer.hole_cards[1].rank=='J' || myPlayer.hole_cards[1].rank=='Q'|| myPlayer.hole_cards[1].rank=='K'|| myPlayer.hole_cards[1].rank=='A' ){
-      bet(gameState.current_buy_in-myPlayer.bet + 10);
-    }
+
+    /*
     if (gameState.current_buy_in < 100) {
       bet(gameState.current_buy_in-myPlayer.bet + 10);
     }
-    bet(0);
+    */
+  } 
+
+  static countPairs() {
+    let counter = 0;
+    for (let i = 0; i < community_cards.length; i++) {
+      for (let j = 0; j < cards.length; j++) {
+        if (community_cards[i].rank == cards[j].rank) {
+          counter += 1;
+        }
+      }
+    }
+    return counter;
   }
 
   static getMyPlayer(gameState) {
